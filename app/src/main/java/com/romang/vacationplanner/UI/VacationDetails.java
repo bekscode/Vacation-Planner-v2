e00c033bab473e96271ac6932d3f9904bcd9cadd
Rebekah Romang <102334386+bekscode@users.noreply.github.com>
2025-09-05 15:01:13 -0400

    Part B1.a - Added menu item for saving vacations

--- app/src/main/java/com/romang/vacationplanner/UI/VacationDetails.java
+++ app/src/main/java/com/romang/vacationplanner/UI/VacationDetails.java
@@ -2,7 +2,9 @@ package com.romang.vacationplanner.UI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +18,12 @@ import com.romang.vacationplanner.R;
 import com.romang.vacationplanner.database.Repository;
 
 public class VacationDetails extends AppCompatActivity {
+    String title;
+    int vacationID;
+    EditText editTitle;
+    EditText editHotel;
+    EditText editVacationStart;
+    EditText editVacationEnd;
     Repository repository;
 
     @Override
@@ -23,7 +31,13 @@ public class VacationDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_vacation_details);
+
         FloatingActionButton fab = findViewById(R.id.fabVacationDetails);
+        editTitle = findViewById(R.id.vacationTitleText);
+        editHotel = findViewById(R.id.hotelNameText);
+        editVacationStart = findViewById(R.id.vacationStartText);
+        editVacationEnd = findViewById(R.id.vacationEndText);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,10 +45,16 @@ public class VacationDetails extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-}
+        @Override
+                public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.menu_vacation_details, menu);
+            return true;
+        }
+    }
