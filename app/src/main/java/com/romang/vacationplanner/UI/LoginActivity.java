54c900b68a747fd1cc5a5169d1b05b9d557a1126
Rebekah Romang <rromang@wgu.edu>
2025-10-21 04:00:27 -0400

    Part B - Updated login to check user database for matches and included working register functionality

--- app/src/main/java/com/romang/vacationplanner/UI/LoginActivity.java
+++ app/src/main/java/com/romang/vacationplanner/UI/LoginActivity.java
@@ -6,25 +6,26 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.romang.vacationplanner.R;
+import com.romang.vacationplanner.database.Repository;
+import com.romang.vacationplanner.entities.User;
+import com.romang.vacationplanner.utils.PasswordUtils;
+
+import java.util.concurrent.Executors;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextInputEditText usernameText, passwordText;
     private Button loginButton;
     private TextView registerTextView;
+    private Repository repository;
 
     // Login for testing
     private final String validUsername = "Admin";
-    private final String validPassword = "Password123";
-
+    private final String validPassword = "Password";
 
 
     @Override
@@ -32,25 +33,44 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        repository = new Repository(getApplication());
         usernameText = findViewById(R.id.usernameText);
         passwordText = findViewById(R.id.passwordText);
         loginButton = findViewById(R.id.loginButton);
         registerTextView = findViewById(R.id.registerTextView);
 
-        loginButton.setOnClickListener(v -> {
-            String username = usernameText.getText().toString().trim();
-            String password = passwordText.getText().toString().trim();
-
-            if (username.equals(validUsername) && password.equals(validPassword)) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-
-            } else {
-                registerTextView.setOnClickListener(v1 -> {
-                    Toast.makeText(this, "Registration not implemented yet.", Toast.LENGTH_LONG).show();
-                });
+        Executors.newSingleThreadExecutor().execute(() -> {
+            if (repository.getUserByUsername("Admin") == null) {
+                User user = new User();
+                user.setUsername("Admin");
+                user.setPasswordHash(PasswordUtils.hashPassword("Password"));
+                repository.insertUser(user);
             }
+        });
+
+        loginButton.setOnClickListener(v -> {
+                    String username = usernameText.getText().toString().trim();
+                    String password = passwordText.getText().toString().trim();
+                    String hash = PasswordUtils.hashPassword(password);
+
+                    Executors.newSingleThreadExecutor().execute(() -> {
+                        User user = repository.getUserByUsername(username);
+                        if (user != null && user.getPasswordHash().equals(hash)) {
+                            runOnUiThread(() -> {
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            });
+                        } else {
+                            runOnUiThread(() -> {
+                                Toast.makeText(this, "Invalid login", Toast.LENGTH_LONG).show();
+                            });
+                        }
+                    });
+        });
+        registerTextView.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
         });
     }
 }
